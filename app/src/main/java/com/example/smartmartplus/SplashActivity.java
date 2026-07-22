01ce5d9328b9afa40dcee770a2d9a3dc451a84c5
ummekulsum29 <ummekulsumzk001@gmail.com>
2026-07-22 16:21:32 +0530

    Initial SmartMartplus project

---
+++ app/src/main/java/com/example/smartmartplus/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.smartmartplus;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        // Delay for 3 seconds and move to LoginActivity
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
+
+            Intent intent = new Intent(
+                    SplashActivity.this,
+                    LoginActivity.class);
+
+            startActivity(intent);
+            finish();
+
+        }, 3000);
+    }
+}
