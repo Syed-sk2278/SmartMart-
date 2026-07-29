43669b5825cd314645d1e3e4a89f4b81f93caaca
ummekulsum29 <ummekulsumzk001@gmail.com>
2026-07-29 17:30:55 +0530

    updated register  screen

--- app/src/main/java/com/example/smartmartplus/RegisterRequest.java
+++ app/src/main/java/com/example/smartmartplus/RegisterRequest.java
@@ -2,16 +2,32 @@ package com.example.smartmartplus;
 
 public class RegisterRequest {
 
+    private String fullName;
     private String email;
+    private String phone;
     private String password;
 
-    public RegisterRequest(String email, String password) {
+    public RegisterRequest(String fullName,
+                           String email,
+                           String phone,
+                           String password) {
+
+        this.fullName = fullName;
         this.email = email;
+        this.phone = phone;
         this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
     }
 
     public String getEmail() {
         return email;
+    }
+
+    public String getPhone() {
+        return phone;
     }
 
     public String getPassword() {
